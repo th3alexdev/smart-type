747588140a26fbca158d4458eb349cf5cddf3bc8
AleKs <alexanderfranciscog@gmail.com>
2023-05-15 12:46:50 -0400

    new tab creation function added

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,3 +1,5 @@
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({ url: "http://localhost:5173/" });
+    // chrome.tabs.create({ url: "chrome-extension://cckjhkecnolheihcboimfcnmfdoimgim/index.html"  });
+
+    chrome.tabs.create({ url: "http://localhost:5173/" })
 });
