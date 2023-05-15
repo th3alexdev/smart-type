31f7232394462d5d047d1d60338f1aac54507835
AleKs <alexanderfranciscog@gmail.com>
2023-05-15 12:42:35 -0400

    new exported components

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,3 @@
-// export {  } from "./"
+export { default as Header } from "./components/Header"
+export { default as HeaderNav } from "./components/HeaderNav"
+export { default as Section } from "./components/Section"
