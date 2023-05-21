2939a4337f470c11ba9829814e2e5ffe577d99b5
AleKs <alexanderfranciscog@gmail.com>
2023-05-21 15:21:47 -0400

    SwitchButton component was added

---
+++ src/components/SwitchButton/SwitchButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SwitchButton() {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider round"></span>
+    </label>
+  )
+}
+
+export default SwitchButton
