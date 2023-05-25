29d166e36eeb1b4fa4be8eb50f0e18639c63a662
AleKs <alexanderfranciscog@gmail.com>
2023-05-24 23:46:35 -0400

    added ShortcutsProvider component to handle shortcuts context

---
+++ src/components/context/ShortcutsProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useEffect } from "react"
+import Manager from "../../classes/ShortcutsManager";
+export const ShortcutsContext = createContext();
+
+
+
+export const ShortcutsProvider = ({ children }) => {
+  const [allShortcuts, setAllShortcuts] = useState([]);
+
+  useEffect(() => {
+    const shortcuts = Manager.getAllShortcuts;
+    setAllShortcuts(shortcuts);
+  }, []);
+
+  return (
+    <ShortcutsContext.Provider value={{ allShortcuts, setAllShortcuts }}>
+      { children }
+    </ShortcutsContext.Provider>
+  )
+}
+
