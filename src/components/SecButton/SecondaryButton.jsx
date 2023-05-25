b63922c503f743a96d7206755979a9c87fc4f18f
AleKs <alexanderfranciscog@gmail.com>
2023-05-25 00:02:22 -0400

    structure was refactored + ShortcutsProvider imported

--- src/components/SecButton/SecondaryButton.jsx
+++ src/components/SecButton/SecondaryButton.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from "react"
+import { ShortcutsContext } from '../context/ShortcutsProvider';
+import { handleImportExport } from "../../helpers/handleImportExport";
+import Manager from "../../classes/ShortcutsManager";
 
 function SecButton({ cta }) {
+  const { allShortcuts, setAllShortcuts } = useContext(ShortcutsContext);
+  
+  const handleClick = () => {
+    handleImportExport(cta, allShortcuts);
+    setAllShortcuts(Manager.getAllShortcuts)
+  }
+
   return (
-    <button className="btn btn-secondary">
+    <button 
+      className="btn btn-secondary"
+      onClick={ (e) => handleClick(cta, allShortcuts) }
+    >
       { cta }
     </button>
   )
