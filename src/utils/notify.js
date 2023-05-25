5166ad4420964816fea526f19409e77b8adde201
AleKs <alexanderfranciscog@gmail.com>
2023-05-24 23:36:00 -0400

    updated function to accept an additional paramater 'emoji'

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,13 +1,13 @@
 import toast from 'react-hot-toast';
 
-const notify = (message) => toast.success(`${message}`, {
+const notify = (message, emoji) => toast.success(`${message}`, {
     duration: 3000,
     position: 'bottom-right',
     style: {
         fontSize: "1rem",
         fontWeight: "500"
     },
-    icon: '👏',
+    icon: emoji,
     iconTheme: {
         primary: '#000',
         secondary: '#fff',
