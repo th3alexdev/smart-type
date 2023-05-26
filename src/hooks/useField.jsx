d897994589fcea907ad56c017380d2023a37fffc
AleKs <alexanderfranciscog@gmail.com>
2023-05-26 14:15:19 -0400

    updated hook classes in the presence of errors

--- src/hooks/useField.jsx
+++ src/hooks/useField.jsx
@@ -3,7 +3,7 @@ export const useField = ({ errors, tag, inputName, type }) => {
     let autoComplete = "off";
 
     if(tag === "input") {
-        className = `form-field form__input ${errors[inputName] ? "input-invalid" : "input-valid"}`
+        className = `form-field form__input ${errors[inputName] ? "input-invalid" : ""}`
 
     } else if(tag === "textarea") {
         className = `form-field form__textarea ${errors[inputName] ? "input-invalid" : ""}`
