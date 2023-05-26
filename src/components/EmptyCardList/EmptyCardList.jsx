4b6e3c43359141032433df0b51ed032b5bb711ed
AleKs <alexanderfranciscog@gmail.com>
2023-05-26 14:43:37 -0400

    updated component to accept a title prop and display the value of the prop

--- src/components/EmptyCardList/EmptyCardList.jsx
+++ src/components/EmptyCardList/EmptyCardList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function EmptyCardList() {
+function EmptyCardList({ title }) {
   return (
     <div className="empty">
       <div className="empty-container">
         <img className="empty__img" src="../../../src/assets/img/empty.png" />
-        <h3 className="empty__title">No shortcuts to show!</h3>
+        <h3 className="empty__title">{ title }</h3>
       </div>
     </div>
   )
