b1bf6d07608a60445767d20c8e6500399fa0d7e0
AleKs <alexanderfranciscog@gmail.com>
2023-05-26 15:39:47 -0400

    added helper with functionalities related to component Card

---
+++ src/helpers/cardHelpers.jsx
@@ -0,0 +1,56 @@
+import { Card } from "../routes"
+
+export const createCardElement = ({ 
+    el, 
+    key, 
+    openCards, 
+    setOpenCards, 
+    setAllShortcuts 
+}) => {
+
+    const isOpen = openCards[key];
+  
+    const toggleCard = () => {
+      if (!isOpen) {
+        setOpenCards((prevState) => ({
+          ...prevState,
+          [key]: !prevState[key],
+        }));
+      }
+    };
+  
+    const closeOtherCards = (currentKey) => {
+      setOpenCards((prevState) => {
+          const updatedOpenCards = {};
+          for (const key in prevState) {
+            if (key !== currentKey) {
+              updatedOpenCards[key] = false;
+            }
+          }
+          return updatedOpenCards;
+      });
+    };
+  
+    const handleClick = () => {
+      if (!isOpen) {
+        closeOtherCards();
+      }
+      toggleCard();
+    };
+  
+    return (
+      <Card
+        name={ el.getName }
+        shortcut={ el.getShortcut }
+        description={ el.getDescription }
+        lastUsedDate={ el.lastUsedDate !== undefined ? el.formatTime(el.lastUsedDate) : null }
+        expansion={ el.getExpansion }
+        timesUsed={ el.timesUsed }
+        key={ key }
+        isOpen={ isOpen }
+        toggleCard={ handleClick }
+        setOpenCards={ setOpenCards }
+        setAllShortcuts={ setAllShortcuts }
+      ></Card>
+    );
+  };
