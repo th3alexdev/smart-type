0b79517cbf0499594c593cdbae79d85cd02f67e1
AleKs <alexanderfranciscog@gmail.com>
2023-05-26 14:59:33 -0400

    refactored in separate components for editing and previewing the cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,39 +1,35 @@
-import React from 'react'
-import { MdSignalCellularAlt } from "react-icons/md"
-import { MdOutlineWatchLater } from "react-icons/md"
-import { MdKeyboardArrowDown } from "react-icons/md"
-import { IconContext } from 'react-icons'
+import React from "react"
+import { IconContext } from "react-icons"
 
+import { CardEditor, CardPreview } from "./components/routes"
 
-function Card({ shortcut, description, timesUsed, lastUsedDate }) {
+function Card( props ) {
+
+  const { isOpen, setOpenCards, toggleCard, ...restProps } = props;
+  
+  const hideCardHandler = () => {
+    setOpenCards({})
+  }
+
   return (
-    <div className="card">
-        <h3 className="card__shortcut">{ shortcut }</h3>
-        <p className="card__command"></p>
-
-        <h2 className="card__description">
-          { description }
-        </h2> 
-
-        <IconContext.Provider value={{ color: "#000000c6", className: "card-icon" }}>
-          <div className="card__stats"> 
-            <div 
-              className="card-pill"
-              title="Times used"
-            ><MdSignalCellularAlt/> { timesUsed }</div>
-            {
-              lastUsedDate && (
-                <div 
-                  className="card-pill"
-                  title="Last used date"
-                ><MdOutlineWatchLater/>  { lastUsedDate }</div>
-              ) 
-            }
-          </div>
-
-          <button className="btn btn-edit card__edit-btn"> <MdKeyboardArrowDown style={{ fontSize: "1.5rem"}}/> </button>
-        </IconContext.Provider>
-    </div>
+   <>
+    <IconContext.Provider value={{ color: "var(--color-icon-color)", className: "card-icon" }}>
+    {
+      isOpen ? (
+        <CardEditor
+          { ...props }
+          setOpenCards={ setOpenCards }
+          closeCard={ hideCardHandler }
+        />
+      ) : (
+        <CardPreview
+          { ...props }
+          openCard={ toggleCard }
+        />
+      )
+    }
+    </IconContext.Provider>
+   </>
   )
 }
 
