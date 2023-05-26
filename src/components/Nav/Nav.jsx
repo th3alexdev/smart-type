7a33981e2c58ab2c712ab7152b553d0f78ac5730
AleKs <alexanderfranciscog@gmail.com>
2023-05-26 14:35:55 -0400

    improved the navigation functionality

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
+import { DropButton } from "../../routes";
+import { SORT_TYPES } from "../../constants/sortTypes";
 
 function Nav({ setSortBy }) {
-
   const sectionHandler = (e) => {
     setSortBy(e.target.dataset.nav)
 
@@ -13,35 +14,72 @@ function Nav({ setSortBy }) {
     e.target.classList.add("nav-active");
   }
 
+  const links = [
+    { label: "Recently Added", nav: SORT_TYPES.RECENTLY_ADDED, url: "/link1"},
+    { label: "Recently Used", nav: SORT_TYPES.RECENTLY_USED, url: "/link2" },
+    { label: "Most Used", nav: SORT_TYPES.MOST_USED, url: "/link3" },
+  ];
+
+  const [selectedOption, setSelectedOption] = useState(links[0].label)
+
+  const selectOption = (link) => {
+    setSelectedOption(link);
+  };
+
   return (
-    <nav className="nav-secondary">
-      <ul className="nav-list nav-list-secondary">
-        <li className="nav-list__item nav-list__item--short">
-          <a 
-            href="#" 
-            className="link nav-active"
-            data-nav="recently-added"
-            onClick={((e) => sectionHandler(e))}
-          >Recently Added</a>
-        </li>
-        <li className="nav-list__item nav-list__item--short">
-          <a 
-            href="#" 
-            className="link"
-            data-nav="recently-used"
-            onClick={((e) => sectionHandler(e))}
-          >Recently Used</a>
-        </li>
-        <li className="nav-list__item nav-list__item--short">
-          <a 
-            href="#"
-            className="link"
-            data-nav="most-used"
-            onClick={((e) => sectionHandler(e))}
-          >Most used</a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <DropButton 
+          selectedOption={ selectedOption }
+          className="dd-nav"
+        >
+          {
+            links.map((link, index) => (
+              link.label !== selectedOption && (
+                <li 
+                  className="dd-menu__item" 
+                  key={ index } 
+                  onClick={ () => selectOption(link.label) }
+                >
+                  <a 
+                    className="link" 
+                    href="#"
+                    onClick={ (e) => sectionHandler(e) }
+                    data-nav={ link.nav }
+                  >{ link.label }</a>
+                </li>
+              )
+            ))
+          }
+      </DropButton>
+      <nav className="nav-secondary">
+        <ul className="nav-list nav-list-secondary">
+          <li className="nav-list__item nav-list__item--short">
+            <a 
+              href="#" 
+              className="link nav-active"
+              data-nav={ SORT_TYPES.RECENTLY_ADDED }
+              onClick={((e) => sectionHandler(e))}
+            >Recently Added</a>
+          </li>
+          <li className="nav-list__item nav-list__item--short">
+            <a 
+              href="#" 
+              className="link"
+              data-nav={ SORT_TYPES.RECENTLY_USED }
+              onClick={((e) => sectionHandler(e))}
+            >Recently Used</a>
+          </li>
+          <li className="nav-list__item nav-list__item--short">
+            <a 
+              href="#"
+              className="link"
+              data-nav={ SORT_TYPES.MOST_USED }
+              onClick={((e) => sectionHandler(e))}
+            >Most used</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
