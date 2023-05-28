bedc1eeecac7a64bbe060e25048e500b640d04e0
AleKs <alexanderfranciscog@gmail.com>
2023-05-27 23:23:50 -0400

    refactored component to handle toggling

--- src/components/SwitchButton/SwitchButton.jsx
+++ src/components/SwitchButton/SwitchButton.jsx
@@ -1,17 +1,16 @@
 import React from "react"
 
-function SwitchButton({ darkTheme, setDarkTheme }) {
-  const toggleTheme = () => {
-    setDarkTheme(!darkTheme);
-    document.body.classList.toggle("dark-mode");
+function SwitchButton({ checked, onChange }) {
+  const handleToggle = () => {
+    onChange(checked);
   };
 
   return (
     <label className="switch">
       <input 
         type="checkbox" 
-        onChange={ toggleTheme } 
-        checked={ darkTheme } 
+        onChange={ handleToggle } 
+        checked={ checked } 
       />
       <span className="slider round"></span>
     </label>
