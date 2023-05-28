e3cf499b97e90c0a6f6e12780b3ed33b879adf83
AleKs <alexanderfranciscog@gmail.com>
2023-05-27 23:31:25 -0400

    component refactored with react-router-dom

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Route, Routes } from "react-router-dom";
 
 import { 
   ManageSection,
@@ -9,50 +10,44 @@ import {
 import { ShortcutsProvider } from "../../context/ShortcutsProvider";
 import { SORT_TYPES } from "../../constants/sortTypes"
 
-function Section({ currentSection, showSection }) {
+function Section({ toggleDarkTheme, darkTheme }) {
 
   const [selectedOption, setSelectedOption] = useState("/"); // change to activeOption
   const [sortBy, setSortBy] = useState(SORT_TYPES.RECENTLY_ADDED);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <ShortcutsProvider>
-    <section className="section">
-      {
-         !showSection || currentSection === "home" ? (
-          // Your shortcuts [Section]
-          <ShortcutsSection
-            {...
-              {
-                sortBy,
-                setSortBy,
-                setIsModalOpen,
-                isModalOpen,
-                selectedOption
+      <section className="section">
+        <Routes>
+          <Route path="/home" element={ (
+              <ShortcutsSection
+              {...
+                {
+                  sortBy,
+                  setSortBy,
+                  setIsModalOpen,
+                  isModalOpen,
+                  selectedOption
+                }
               }
-            }
-          />  
-
-        ) : currentSection === "test" ? (
-          // Test your shortcuts [Section]
-          <TestSection /> 
-
-        ) : (
-          // Manage shortcuts [Section] 
-          <ManageSection
-            {...
-              {
-                setSelectedOption,
-                selectedOption,
-                darkTheme,
-                setDarkTheme,
+            />  
+          ) } />
+          <Route path="/test" element={ <TestSection /> } />
+          <Route path="/manage" element={ 
+              <ManageSection
+              {...
+                {
+                  setSelectedOption,
+                  selectedOption,
+                  darkTheme,
+                  toggleDarkTheme,
+                }
               }
-            }
-          /> 
-
-        )}
-    </section>
+              /> 
+          } />
+        </Routes>
+      </section>
     </ShortcutsProvider>
   )
 }
