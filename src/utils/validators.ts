15bfbe22b4bdecffa9a044c034a43d92fbe63f8f
AleKs <alexanderfranciscog@gmail.com>
2023-05-27 23:01:52 -0400

    refactor to add onSubmitEdit function + code improved

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,24 +1,26 @@
 import Manager from "../classes/ShortcutsManager";
 import { Shortcut } from "../classes/Shortcut";
+import notify from "./notify";
 
-export const onSubmit = ({ 
+export const onSubmitAdd = ({ 
   data, 
   setError, 
-  setOpenModal, 
-  clearErrors 
-
+  clearErrors,
+  isEditing = false,
 }: {
   data: {
     name: string;
     description: string;
     expansion: string;
+    originalName?: string;
   };
   setError: Function;
-  setOpenModal: Function;
   clearErrors: Function;
-
+  isEditing?: boolean;
 }): void => {
-  let allShortcuts: Array<Shortcut>, alreadyExist: boolean;
+  let allShortcuts: Array<Shortcut>, 
+      alreadyExist: boolean,
+      coincidence: Shortcut | undefined;
 
   allShortcuts = Manager.getAllShortcuts;
   alreadyExist = allShortcuts.some(
@@ -27,12 +29,32 @@ export const onSubmit = ({
 
   clearErrors(); 
 
-  if(alreadyExist) {
+  if(alreadyExist && !isEditing) {
     setError("name", { 
         type: "already exist", 
         message: "Shortcut name already exists"
     });
     return;
+  }
+
+  if (isEditing) {
+    const newShortcutName = data.originalName; // Nuevo nombre del shortcut
+
+    coincidence = allShortcuts.find(
+      (shortcut) => shortcut.getName === newShortcutName
+    );
+
+
+    if (coincidence) {
+      Manager.editShortcut({
+        fullShortcut: coincidence,
+        name: data.name,
+        description: data.description,
+        expansion: data.expansion
+      });
+    }
+
+    return
   }
 
   const newShortcut = new Shortcut({
@@ -46,6 +68,57 @@ export const onSubmit = ({
 
   Manager.addShortcut(newShortcut);
 
-  setOpenModal(false);
   alreadyExist = false;
+
+  !isEditing && (notify("Shortcut added successfully", "👏"))
 }
+
+
+export const onSubmitEdit = ({ 
+  data, 
+  setError, 
+  clearErrors,
+}: {
+  data: {
+    name: string;
+    description: string;
+    expansion: string;
+  };
+  setError: Function;
+  clearErrors: Function;  
+}): void => {
+  let allShortcuts: Array<Shortcut>, 
+      coincidence: Shortcut | undefined,
+      fullShortcut: Shortcut,
+      alreadyExist: boolean
+
+  allShortcuts = Manager.getAllShortcuts;
+
+  coincidence = allShortcuts.find((shortcut) => {
+    return shortcut.getName === data.name;
+  });
+
+  clearErrors();
+
+  if(coincidence) {
+    fullShortcut = coincidence;
+
+    if(fullShortcut.getName !== data.name) {
+      Manager.editShortcut({ fullShortcut, name: data.name })
+    }
+
+    if(fullShortcut.getDescription !== data.description) {
+      Manager.editShortcut({ fullShortcut, description: data.description })
+    }
+
+    if(fullShortcut.getExpansion !== data.expansion) {
+      Manager.editShortcut({ fullShortcut, expansion: data.expansion })
+    }
+
+  } else {
+    Manager.removeShortcut(data.name)
+    onSubmitAdd({ data, setError, clearErrors, isEditing: true })
+  }
+
+  notify("Shortcut updated successfully", "🔄")
+};
