f47c796485c65ece67d6489925a925b4aa13ef59
AleKs <alexanderfranciscog@gmail.com>
2023-05-29 19:30:24 -0400

    removed unnecessary console.log

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
       setDarkTheme(darkModeEnabled);
       toggleDarkTheme(darkModeEnabled)
     }
-    console.log(currentSection)
   }, []);
 
   const toggleDarkTheme = (enabled) => {
