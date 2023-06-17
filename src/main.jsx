aca223bc4562d93b8fa065dd2f0bda1c7e46fe2b
AleKs <alexanderfranciscog@gmail.com>
2023-06-17 18:18:08 -0400

    refactored component to add ShortcutsProvider context

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
+import { ShortcutsProvider } from "./context/ShortcutsProvider"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ShortcutsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ShortcutsProvider>
 )
