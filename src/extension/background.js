3929c51c0fa0318fcdf723f0b3cad58f688531ce
AleKs <alexanderfranciscog@gmail.com>
2023-06-29 23:28:27 -0400

    added a listener for receiving messages

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,5 +1,15 @@
+// This listener is triggered when the extension button is clicked.
 chrome.action.onClicked.addListener(() => {
-    // chrome.tabs.create({ url: "chrome-extension://cckjhkecnolheihcboimfcnmfdoimgim/index.html"  });
 
-    chrome.tabs.create({ url: "http://localhost:5173/" })
+    // Create a new tab with the specified URL.
+    chrome.tabs.create({ url: "http://localhost:5173/home" })
+});
+
+// This listener is triggered when a message is received from the content script.
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "current-tab") {
+      // The content script has requested information about the current tab.
+      // Send the tab information back to the content script.
+      sendResponse({tab: sender.tab});
+  }
 });
