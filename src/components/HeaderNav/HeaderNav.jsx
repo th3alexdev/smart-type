f4803855ca043c8898a0c09b1d37ec21935283fa
AleKs <alexanderfranciscog@gmail.com>
2023-07-04 01:46:56 -0400

    refactored navigation links

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -5,10 +5,12 @@ import { Link } from "react-router-dom";
 
 function HeaderNav({ setCurrentSection, openNav, setOpenNav, currentSection }) {
 
+  // Toggle the navigation state (open/close)
   const toggleNav = () => {
     setOpenNav(!openNav)
   }
 
+  // Handler for setting the current section and toggling the navigation
   const setSectionHandler = (e) => {
     toggleNav()
     setCurrentSection(e.currentTarget.dataset.section);
@@ -19,25 +21,25 @@ function HeaderNav({ setCurrentSection, openNav, setOpenNav, currentSection }) {
       <ul className="nav-list">
         <li className="nav-list__item">
           <Link 
-            to="/home" 
-            className={`link ${currentSection === "/home" && "nav-header-active"}`}
-            data-section="/home"
+            to="/index.html#home" 
+            className={`link ${currentSection === "/index.html/home" && "nav-header-active"}`}
+            data-section="/index.html/home"
             onClick={((e) => setSectionHandler(e))}
           >Your shortcuts</Link>
         </li>
         <li className="nav-list__item">
           <Link
-            to="/test" 
-            className={`link ${currentSection === "/test" && "nav-header-active"}`}
-            data-section="/test"
+            to="/index.html#test" 
+            className={`link ${currentSection === "/index.html/test" && "nav-header-active"}`}
+            data-section="/index.html/test"
             onClick={((e) => setSectionHandler(e))}
           >Test your shortcuts</Link>
         </li>
         <li className="nav-list__item">
           <Link 
-            to="/manage"
-            className={ currentSection === "/manage" ? "link nav-header-active" : "link" }
-            data-section="/manage"
+            to="/index.html#manage"
+            className={ currentSection === "/index.html/manage" ? "link nav-header-active" : "link" }
+            data-section="/index.html/manage"
             onClick={((e) => setSectionHandler(e))}
           >Manage shortcuts</Link>
         </li>
