a4ebd833963d33a0a5db0f341a418bff7fe6daab
AleKs <alexanderfranciscog@gmail.com>
2023-07-06 20:19:30 -0400

    updated event listeners and storage retrieval

--- public/extension/background.js
+++ public/extension/background.js
@@ -1,15 +1,58 @@
 // This listener is triggered when the extension button is clicked.
 chrome.action.onClicked.addListener(() => {
-
-    // Create a new tab with the specified URL.
-    chrome.tabs.create({ url: "chrome-extension://lfhbffpcihffklieljkgngecbllpalhg/index.html" })
+  // Create a new tab with the specified URL.
+  chrome.tabs.create({ url: "chrome-extension://lfhbffpcihffklieljkgngecbllpalhg/index.html" })
 });
 
-// This listener is triggered when a message is received from the content script.
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "current-tab") {
-      // The content script has requested information about the current tab.
-      // Send the tab information back to the content script.
-      sendResponse({tab: sender.tab});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+if (request.action === 'sendVariable' && request.action === 'sendCommand') {
+  const currentTabUrl = sender.tab.url;
+  // console.log(currentTabUrl)
+
+  const shortcuts = request.data
+  const command = request.command
+
+  // console.log('shortcuts ', shortcuts);
+  // console.log('commands ', command);
+  // console.log('current url: ', currentTabUrl);
+
+  // Check if the current tab URL starts with the dashboard URL
+  if(!currentTabUrl.startsWith(dashboardURL)) {
+
+    chrome.storage.sync.get(["shortcuts","command"], (result) => {
+      const shortcuts = result.shortcuts;
+      const command = result.command;
+
+      // Check if shortcuts were found in chrome.storage
+      if (shortcuts) {
+        // console.log(shortcuts)
+        handleInputEvent(shortcuts, command);
+      }
+  });
+  }
+}
+});
+
+chrome.storage.sync.get(['shortcuts', 'command'], function(result) {
+  // Maneja los valores obtenidos
+  const shortcuts = result.shortcuts;
+  const command = result.command;
+
+  // Realiza las acciones necesarias con los valores obtenidos
+  console.log('Shortcuts:', shortcuts);
+  console.log('Command:', command);
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // Comprueba la acción solicitada por el mensaje
+  if (request.action === 'getShortcuts') {
+    // Obtiene los datos de los atajos desde chrome.storage.sync
+    chrome.storage.sync.get(['shortcuts', 'command'], function(result) {
+      // Envia los datos de los atajos al script de contenido
+      sendResponse({ shortcuts: result.shortcuts, command: result.command });
+    });
+
+    // Indica que se enviará una respuesta asincrónica
+    return true;
   }
 });
